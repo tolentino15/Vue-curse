45fdb55efe712fcf899a7dce3aa761c9b42de4fc
Luiz Eduardo Tolentino <luiztm10@gmail.com>
2024-06-21 17:01:05 -0300

    Testes Unitarios - Media e Situacao do Aluno

--- teste-unitario/testes-unitarios/src/components/Alunos.vue
+++ teste-unitario/testes-unitarios/src/components/Alunos.vue
@@ -7,5 +7,26 @@
     //Variaveis
     let nome = ref('Luiz');
 
+    //Funcao que retorna a media do aluno
+    function mediaAluno(nota1, nota2){
+            return (nota1+nota2)/2;
+    }
+
+    //situacao do aluno
+    function situacao(media){
+        let situacaoAluno = '';
+
+        if(media >= 7){
+            situacaoAluno = 'Aprovado!'
+        } else if (media >= 5){
+            situacaoAluno = 'Na Final!'
+        }else{
+            situacaoAluno = 'Reprovado!'
+        }
+
+
+        return situacaoAluno
+    }
+
 
 </script>
--- teste-unitario/testes-unitarios/src/components/__tests__/Alunos.spec.ts
+++ teste-unitario/testes-unitarios/src/components/__tests__/Alunos.spec.ts
@@ -17,6 +17,26 @@ describe('Alunos', () => {
 
             expect(componente.vm.nome).toBe('Luiz')
 
+    });
+
+    it('Verificando media do Aluno', () =>{
+
+        //obter o componente
+        const componente = mount(Alunos);
+
+        //Teste
+        expect(componente.vm.mediaAluno(7,9)).toBe(8);
+
+    })
+
+    it('Verificando situacao do Aluno', () =>{
+
+        //obter o componente
+        const componente = mount(Alunos);
+
+        //Teste
+        expect(componente.vm.situacao(8)).toBe('Aprovado!');
+
     })
 
 });
